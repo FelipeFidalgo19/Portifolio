a0b96e217b90979719829a8bb498a30af06e2937
FelipeFidalgo19 <felipefidalgo100@gmail.com>
2024-06-05 17:06:38 -0300

    grifar

--- src/components/Sobre/index.js
+++ src/components/Sobre/index.js
@@ -5,6 +5,10 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three-stdlib';
 import './sobre.module.scss';
 
+const Green = ({children}) => {
+    return <span style={{color:'#32a852'}}>{children}</span>
+}
+
 const ThreeScene = () => {
     const mountRef = useRef(null);
     const modelRef = useRef(null);
@@ -53,7 +57,6 @@ const ThreeScene = () => {
                 modelRef.current.rotation.y += 0.01; // Ajuste a velocidade de rotação conforme necessário
                 if(camera.position.y!=0.7799999999999994){
                     camera.position.y -= 0.01; 
-                    console.log(camera.position.y);
                 }
             }
         }
@@ -95,6 +98,8 @@ const ThreeScene = () => {
     );
 };
 
+
+
 export default function Sobre() {
     return (
         <Container className="cont" fixed>
@@ -110,10 +115,10 @@ export default function Sobre() {
                     <h1>Bem-vindo ao meu site de portfólio!</h1>
                     <hr />
                     <p>
-                        Sou um desenvolvedor full-stack apaixonado por tecnologia, e estou animado por ter a oportunidade de compartilhar meu trabalho e minha jornada com você.<br /><br />
-                        Desde jovem, sempre fui fascinado pela tecnologia e sua capacidade de transformar a maneira como vivemos e trabalhamos. Como desenvolvedor full-stack, tenho o privilégio de trabalhar com tecnologias inovadoras e colaborar em projetos empolgantes. Ao longo dos anos, adquiri habilidades em diversas linguagens de programação e plataformas, incluindo React Native, React js, JavaScript, PHP, Node.js, C#, Python, entre outros.<br /><br />
-                        Acredito que a tecnologia tem o poder de criar soluções transformadoras que impactam a vida das pessoas de maneiras positivas. Para mim, a criatividade e a inovação são essenciais para criar aplicativos e softwares que resolvam problemas complexos de forma eficiente.<br /><br />
-                        Neste site, você encontrará exemplos dos projetos em que trabalhei, incluindo aplicações móveis criadas com React Native, web apps desenvolvidos com React js, e soluções back-end construídas com Node.js, PHP e C#. Também tenho experiência em trabalhar com a linguagem de programação Python em projetos de análise de dados e machine learning.<br /><br />
+                        Sou um <Green>desenvolvedor full-stack</Green> apaixonado por tecnologia, e estou animado por ter a oportunidade de compartilhar meu trabalho e minha jornada com você.<br /><br />
+                        Desde jovem, sempre fui fascinado pela tecnologia e sua capacidade de transformar a maneira como vivemos e trabalhamos. Como desenvolvedor full-stack, tenho o privilégio de trabalhar com <Green>tecnologias inovadoras</Green> e colaborar em projetos empolgantes. Ao longo dos anos, adquiri habilidades em diversas linguagens de programação e plataformas, incluindo React Native, React js, JavaScript, PHP, Node.js, C#, Python, entre outros.<br /><br />
+                        Acredito que a tecnologia tem o poder de <Green>criar soluções transformadoras</Green> que impactam a vida das pessoas de maneiras positivas. Para mim, a criatividade e a inovação são essenciais para criar aplicativos e softwares que resolvam problemas complexos de forma eficiente.<br /><br />
+                        Neste site, você encontrará <Green>exemplos dos projetos</Green> em que trabalhei, incluindo aplicações móveis criadas com React Native, web apps desenvolvidos com React js, e soluções back-end construídas com Node.js, PHP e C#. Também tenho experiência em trabalhar com a linguagem de programação Python em projetos de análise de dados e machine learning.<br /><br />
                         Espero que você aprecie o meu trabalho e sinta-se à vontade para entrar em contato comigo para discutir oportunidades de colaboração ou simplesmente compartilhar sua opinião.<br /><br />
                         Obrigado por visitar meu site de portfólio!<br /><br />
                     </p>
